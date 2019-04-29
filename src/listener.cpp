fb7dcaebfba83f09944e2d71f475083671d37309
Gabriel Castro <gabriel1997.castro@gmail.com>
2019-04-29 15:59:29 -0300

    Add listener to velocities

---
+++ src/listener.cpp
@@ -0,0 +1,32 @@
+/*
+ * Author: Anis Koubaa for Gaitech EDU
+ * Year: 2016
+ *
+ */
+
+#include "ros/ros.h"
+#include "rover/WheelVel.h"
+
+void WheelsVelocityCallback(const rover::WheelVel vel)
+{
+    ROS_INFO("[Listener] I heard: [%f]\n", vel.left_wheels);
+    ROS_INFO("[Listener] I heard: [%f]\n", vel.right_wheels);
+}
+
+int main(int argc, char **argv)
+{
+    // Initiate a new ROS node named "listener"
+	ros::init(argc, argv, "listener_vel");
+	//create a node handle: it is reference assigned to a new node
+	ros::NodeHandle node;
+
+
+    // Subscribe to a given topic, in this case "chatter".
+	//chatterCallback: is the name of the callback function that will be executed each time a message is received.
+    ros::Subscriber sub = node.subscribe("wheels_velocity", 10, WheelsVelocityCallback);
+
+    // Enter a loop, pumping callbacks
+    ros::spin();
+
+    return 0;
+}
